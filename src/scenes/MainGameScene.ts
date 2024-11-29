81ed479870028464531b2dfbd9dda31cc45eba7c
Akilah Jackson <akilah@mindmapengineers.com>
2024-11-28 19:17:57 -0800

    Update MainGameScene.ts

--- src/scenes/MainGameScene.ts
+++ src/scenes/MainGameScene.ts
@@ -9,7 +9,7 @@ export class MainGameScene extends Phaser.Scene {
     console.log('Creating main game scene...');
     const { width, height } = this.cameras.main;
     
-    const text = this.add.text(width / 2, height / 2, 'Harvest Slots', {
+    const text = this.add.text(width / 2, height / 2, 'Rewarding Harvest', {
       fontSize: '32px',
       color: '#4AE54A'
     });
