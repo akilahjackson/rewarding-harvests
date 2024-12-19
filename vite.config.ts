28cc0812674f0f36d0663df35b348901b48f11fa
Akilah Jackson <22052258-akilahjackson@users.noreply.replit.com>
2024-12-19 04:17:52 +0000

    comment oit lovable tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { componentTagger } from "lovable-tagger";
+//import { componentTagger } from "lovable-tagger";
 import path from 'path';
 
 ///comments section
