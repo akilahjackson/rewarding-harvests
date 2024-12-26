25f355e5393fa1536c1e673aed971a0947901854
Akilah Jackson <22052258-akilahjackson@users.noreply.replit.com>
2024-12-26 06:47:01 +0000

    update

--- replit.nix
+++ replit.nix
@@ -1,5 +1,22 @@
-{pkgs}: {
+
+{ pkgs }: {
   deps = [
     pkgs.nodePackages.parcel
+    pkgs.killall
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.pkg-config
+    pkgs.cairo
+    pkgs.pixman
+    pkgs.libsodium
   ];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.cairo
+      pkgs.pixman
+      pkgs.libsodium
+    ];
+  };
 }
